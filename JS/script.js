60f313e14ff3d5645dc2b0b28893065025d0e314
Ariel Vegas <vgasariel@gmail.com>
2023-09-27 15:27:05 -0300

    Contenido dinámico de página principal con JS. Borramos el contenido estático de ejemplo

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+const main = document.querySelector("main");
+let productos;
+
+const cargarProductos = async () => {
+    const response = await fetch("JS/productos.json");
+    productos = await response.json()
+    localStorage.setItem("productos", JSON.stringify(productos));    
+}
+
+productos = localStorage.getItem("productos");
+if (productos == null) {
+    cargarProductos();
+} else if (typeof productos == "string") {
+    productos = JSON.parse(productos);
+}
+
+productos.forEach(producto => {
+    let item = document.createElement("div");
+    item.classList.add("producto");
+    item.innerHTML = (
+        `<p>${producto.nombre}</p>
+        <p>${producto.descripcion}</p>`
+    )
+    
+    let boton = document.createElement("button");
+    boton.classList.add("producto_boton", "bx", "bx-chevrons-right");
+    boton.addEventListener("click", () => {
+        sessionStorage.setItem("id", producto.nombre);
+        window.location.href = "detalles.html";
+    })
+
+    item.appendChild(boton);
+    main.appendChild(item);
+});
